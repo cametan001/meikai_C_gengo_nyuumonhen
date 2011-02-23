fe97aaf4c2827a38ecda21e86ff941d5f9a6236a
cametan001 <Masashi.Kameda+github@gmail.com>
2011-02-23 20:47:08 +0900

    2011年2月23日午後8:47

---
+++ Chapter1/1-3/Ex1-6/ex1-6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+  puts("風");
+  puts("林");
+  puts("火");
+  puts("山");
+
+  return (0);
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Chapter1/1-3/Ex1-8/ex1-8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n1, n2, n3;
+
+  puts("三つの整数を入力してください。");
+  printf("整数1:"); scanf("%d", &n1);
+  printf("整数2:"); scanf("%d", &n2);
+  printf("整数3:"); scanf("%d", &n3);
+
+  printf("それらの和は%dです。\n", n1 + n2 + n3);
+
+  return (0);
+}
